c7d98d3136788d6b440772acc1b182a757608ebd
Dan Veenstra <danielveenstra11@gmail.com>
2015-08-15 18:20:48 -0500

    Removed nonsense files

--- CreateDatabase.sh
+++
@@ -1,6 +0,0 @@
-cp /etc/postgresql/9.3/main/pg_hba.conf /etc/postgresql/9.3/main/pg_hba.conf_bck
-cp pg_hba.conf /etc/postgresql/9.3/main/pg_hba.conf
-service postgresql restart
-createuser wsss
-createdb -e -O wsss wsss
-
