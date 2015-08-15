a24884de841b6e61a4678576c92130d6d2752f93
Alex Stein <astein.ny@gmail.com>
2015-08-15 18:16:30 -0500

    started working on script to parse speeches from C-SPAN .dfxp files

--- video_to_text/post_process.py
+++ video_to_text/post_process.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python
+# Usage: python post_process.py source.dfxp output{.txt}
 
+import tt2srt
 import sys
+import os
 
 def is_number(s):
     try:
@@ -9,14 +12,21 @@ def is_number(s):
     except ValueError:
         return False
 
-filename = sys.argv[1]
+temp_dest = sys.argv[2] + '_tmp.srt'
+tt2srt.translate(sys.argv[1], temp_dest)
 
-f = open(filename, "r")
-lines = f.readlines()
-f.close()
+srt_file = open(temp_dest, 'r')
+text = srt_file.readlines()
+lines_to_remove = []
 
-f = open(filename, "w")
-for line in lines:
-  	if not is_number(line[0]):
-    	f.write(line)
-f.close()
+for line in text:
+    if (is_number(line[0]) or (line[0] == '\n')):
+        lines_to_remove.append(line);
+
+for line in lines_to_remove:
+    text.remove(line)
+
+print text[0:30]
+
+srt_file.close()
+# os.remove(temp_dest)
