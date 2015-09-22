ba4c75eefefbe318c024fba4151c8641148f1852
Dan Veenstra <danielveenstra11@gmail.com>
2015-09-21 20:33:42 -0500

    Test Case updates and init.py

--- test/Test_Transcriber.py
+++ test/Test_Transcriber.py
@@ -1,17 +1,24 @@
 import unittest2 as unittest
 #import transcriber from transcriber.py
 import os.path
-from .. import Transcriber
-from transcriber.py import transcriber 
-class Test_Transcriber(unittest.TestCase):
+import sys
+from os import path
+sys.path.append( path.dirname( path.dirname( path.abspath(__file__) ) ) )
+from Transcriber.transcriber import Transcriber
 
+#Run from the WeSaidSheSaid root directory
+#python test/Test_Transcriber
+
+class Test_Transcriber(unittest.TestCase):
+	TestInstance = Transcriber()
+	TestInstance.transcribe("http://www.c-span.org/video/?326471-1/hillary-clinton-presidential-campaign-announcement","TestSpeech")
 	
 	def test_speech_download(self):
-		#Need to write the code that calls the downloader for a sample website
-		self.assertTrue(os.path.isfile("../data/TestSpeech.en.dfxp"))
+		#Checks that the downloader downloaded a dfxp file. 
+		self.assertTrue(os.path.isfile("Transcriber/data/TestSpeech.en.dfxp"))
 
 	def test_dfxp_to_srt(self):
-		pass
+		self.assertTrue(os.path.isfile("Transcriber/data/TestSpeech_tmp.srt"))
 	def test_srt_to_final(self):
 		pass
 
