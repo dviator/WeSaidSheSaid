660cc25a18ad5ecbe85a2d01c2530ea9a6967d87
Dan Veenstra <danielveenstra11@gmail.com>
2015-09-28 20:51:00 -0500

    Got crawler grabbing links to videos, wrote bones of parsing function for speech videos.

--- Crawler/Crawler/spiders/cspan_spider.py
+++ Crawler/Crawler/spiders/cspan_spider.py
@@ -16,12 +16,43 @@ class CspanSpider(scrapy.Spider):
 	def parse(self, response):
 		filename = response.url.split("/")[-2] + '.html'
 		print filename
-		# with open(filename, 'wb') as f:
-		# 	f.write(response.body)
+		with open(filename, 'wb') as f:
+			f.write(response.body)
+		#Extract the links to the search results on the page.
 		speechLinks = response.xpath('//section/ul/li/a/@href').extract()
-		print speechLinks
-		with open("links.txt", 'wb') as f:
-			f.write(str(speechLinks))
+		# print speechLinks
+		# with open("links.txt", 'wb') as f:
+		# 	f.write(str(speechLinks))
+
+		#Generate a new crawl request for each link followed that treats them as speech videos. 
+		for url in speechLinks:
+			url = response.urljoin(url)
+			yield scrapy.Request(url, callback=self.parse_speech_page)
+
+
+	#This function handles those pages which are sent as candidates for presidential campaign speeches.
+	def parse_speech_page(self, response):
+		print response.headers
+		print response.url
+		
+		#Check that this is indeed a video page
+		pageType = response.xpath('body/@class').extract()
+		print pageType
+		if pageType[0] == 'video event':
+			print "It's a video!"
+
+		#Gather video metadata
+
+		#Call Transcriber class
+
+		#Either add to item to collect as one feed or call insert to database function.
+
+
+		#Prints the html of the webpage as a text file in current directory. Useful for finding the paths to follow.
+		#Use with sublime's reindent feature for easy to follow html
+		#pageTitle = response.url.split("/")[-1] + '.html'
+		# with open(pageTitle, 'wb') as f:
+		# 	f.write(str(response.body))
 
 
 
