a896835698b1b9d284c64ec51cabc20d22d79f69
Alex Stein <astein.ny@gmail.com>
2015-10-25 18:20:07 -0500

    redirected all output from crawler into log under spiders/logs

--- wesaidshesaid/Crawler/Crawler/spiders/cspan_spider.py
+++ wesaidshesaid/Crawler/Crawler/spiders/cspan_spider.py
@@ -2,6 +2,7 @@ import scrapy
 from scrapy.loader import ItemLoader
 from Crawler.items import CSPANItem
 import psycopg2
+import logging
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait 
@@ -12,9 +13,7 @@ import datetime
 import os
 import sys
 root = os.environ['WSSS_ROOT']
-print root
 tpath = os.path.join(root, root + '/wesaidshesaid/Transcriber')
-print tpath
 sys.path.insert(0,tpath)
 # import transcriber
 from transcriber import Transcriber
@@ -27,10 +26,10 @@ validCandidates = []
 for record in cur:
 	for array in record:
 		for name in array:
-			print name
+			# self.logger.debug(name)
 			validCandidates.append(name)
-#print validCandidates
-print validCandidates
+#print valid candidates
+# self.logger.debug(validCandidates)
 conn.commit()
 cur.close()
 conn.close()
@@ -43,6 +42,9 @@ class CspanSpider(scrapy.Spider):
 	def __init__(self):
 		self.driver = webdriver.Firefox()
 
+		### Exterior Print statements moved here for use of logger
+		self.logger.debug("root is: " + str(root))
+		self.logger.debug("tpath is: " + str(tpath))
 
 	def parse(self, response):
 		#With selenium, click the show more videos button until it is no longer possible to do so. 
@@ -57,19 +59,19 @@ class CspanSpider(scrapy.Spider):
 				element = WebDriverWait(self.driver, 30).until(EC.presence_of_element_located((By.ID, "loadmore")))
 				next = self.driver.find_element_by_id('loadmore')
 				next.click()
-				print "Clicking!"
+				self.logger.debug("Clicking!")
 				
 				#This break is a shortcut for Development. Comment out to get more videos
-				#break
+				break
 			except:
 				break
 
-		print "Scrapy URL is: ", response.url
+		self.logger.debug("Scrapy URL is: " + str(response.url))
 
 		
 		url = self.driver.current_url
 
-		print "Selenium URL is: ", url
+		self.logger.debug("Selenium URL is: " +  str(url))
 
 		# with open("seleniumResponse.txt", 'wb') as f:
 		# 	f.write(Seleniumresponse.encode('utf-8'))
@@ -77,25 +79,25 @@ class CspanSpider(scrapy.Spider):
 		
 
 		# filename = response.url.split("/")[-2] + '.html'
-		# print filename
+		# self.logger.debug(filename)
 		# with open(filename, 'wb') as f:
 		# 	f.write(response.body)
 		speechLinks = []
 		#Extract from the selenium page source instead
 		speechLinkElements = self.driver.find_elements_by_xpath('//section/ul/li/a')
-		print "Elements list is ", speechLinkElements
+		self.logger.debug("Elements list is " + str(speechLinkElements))
 		for s in speechLinkElements:
-			#print "Attribute is: ", s.get_attribute('href')
+			#self.logger.debug("Attribute is: ", s.get_attribute('href'))
 			speechLinks.append(s.get_attribute('href'))
 
-		print "Number of Links is: ", len(speechLinks)
+		self.logger.debug("Number of Links is: " + str(len(speechLinks)))
 
 		#Extract the links to the search results on the page.
 		#speechLinks = response.xpath('//section/ul/li/a/@href').extract()
-		# print speechLinks
+		# self.logger.debug(speechLinks)
 		# with open("links.txt", 'wb') as f:
 		# 	f.write(str(speechLinks))
-		print speechLinks
+		self.logger.debug(speechLinks)
 
 		#Generate a new crawl request for each link followed that treats them as speech videos. 
 		for url in speechLinks:
@@ -125,23 +127,23 @@ class CspanSpider(scrapy.Spider):
 	def validate_speaker(self, speakers):
 		for speaker in speakers:
 			if speaker in validCandidates:
-				print speaker, "matches!"
+				self.logger.debug(str(speaker) + " matches!")
 				return speaker
 			else:
-				print speaker, "doesn't match!"
+				self.logger.debug(str(speaker) + " doesn't match!")
 		return None
 
 
 	#This function handles those pages which are sent as candidates for presidential campaign speeches.
 	def parse_speech_page(self, response):
-		print response.headers
-		print response.url
+		self.logger.debug(response.headers)
+		self.logger.debug(response.url)
 		
 		#Check that this is indeed a video page
 		pageType = response.xpath('body/@class').extract()
-		print pageType
+		self.logger.debug(pageType)
 		if pageType[0] == 'video event':
-			print "It's a video!"
+			self.logger.debug("It's a video!")
 
 			#Gather video metadata into a CSPAN Item
 			l = ItemLoader(item=CSPANItem(), response=response)
@@ -172,7 +174,7 @@ class CspanSpider(scrapy.Spider):
 
 			#Validate that the item contains a speaker we're interested in.
 			item['speaker'] = self.validate_speaker(item['speaker'])
-			print "speaker is: ", item['speaker']
+			self.logger.debug("speaker is: " + str(item['speaker']))
 			
 			#Write gathered data to the database
 			if item['speaker'] is not None :
@@ -196,10 +198,10 @@ class CspanSpider(scrapy.Spider):
 		# with open(pageTitle, 'wb') as f:
 		# 	f.write(str(response.body))
 	# def parse(self, response):
-	# 	print response
-	# 	print response.url.split("/")
+	# 	self.logger.debug(response)
+	# 	self.logger.debug(response.url.split("/"))
 	# 	filename = response.url.split("/")[-1] + '.html'
-	# 	print filename
+	# 	self.logger.debug(filename)
  #        f = open(filename, 'wb')
  #        f.write(response.body)
  #        #f.close()
