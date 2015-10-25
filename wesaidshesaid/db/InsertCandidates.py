27a5c6e70cdc5ad3c3bde635791d8effb89f3a77
Dan Veenstra <danielveenstra11@gmail.com>
2015-10-25 18:14:30 -0500

    Added Martin O'Malley to Candidates and rearranged Insert Candidates to be more robust

--- wesaidshesaid/db/InsertCandidates.py
+++ wesaidshesaid/db/InsertCandidates.py
@@ -6,32 +6,36 @@ cur = conn.cursor()
 #Have to figure out how to escape O'Malley
 # ('Martin J. O'Malley', 'Martin', "O'Malley", 'J.','Democrat'),
 
-cur.execute("""INSERT INTO Candidates (fullName, validNames, first, last, middle, party) VALUES
-	('Marco Rubio', ARRAY['Marco Rubio', 'Senator Marco Rubio'],'Marco', 'Rubio','','Republican'),
-	('Carly Fiorina', ARRAY['Carly Fiorina', 'Mrs. Carly Fiorina'], 'Fiorina', 'Carly','','Republican'),
-	('Ted Cruz', ARRAY['Ted Cruz', 'Senator Ted Cruz'], 'Cruz', 'Ted','','Republican'),
-	('Scott Walker', ARRAY['Scott Walker', 'Gov. Scott Walker', 'Governor Scott Walker'], 'Walker', 'Scott','','Republican'),
-	('Bernie Sanders', ARRAY['Bernie Sanders', 'Senator Bernie Sanders', 'Bernard Sanders'], 'Sanders', 'Bernie','','Democrat'),
-	('Hillary Rodham Clinton', ARRAY['Hillary Rodham Clinton', 'Hillary Clinton'], 'Clinton', 'Hillary', 'Rodham', 'Democrat'),
-	('John R. Kasich', ARRAY['John R. Kasich', 'Governor John R. Kasich', 'John Kasich', 'Governor John Kasich'], 'Kasich', 'John', 'R.', 'Republican'),
-	('Chris Christie', ARRAY['Chris Christie', 'Governor Chris Christie'], 'Christie', 'Chris','','Republican'),
-	('Ben Carson', ARRAY['Ben Carson', 'Benjamin Carson, Sr.', 'Dr. Ben Carson', 'Ben Carson, M.D.'], 'Carson', 'Ben','','Republican'),
-	('Jeb Bush', ARRAY['Jeb Bush'], 'Bush', 'Jeb','','Republican'),
-	('Jim Gilmore', ARRAY['Jim Gilmore'], 'Gilmore', 'Jim','','Republican'),
-	('Lindsey Graham', ARRAY['Lindsey Graham'], 'Graham', 'Lindsey','','Republican'),
-	('Mike Huckabee', ARRAY['Mike Huckabee'], 'Huckabee', 'Mike','','Republican'),
-	('Bobby Jindal', ARRAY['Bobby Jindal'], 'Jindal', 'Bobby','','Republican'),
-	('George Pataki', ARRAY['George Pataki'], 'Pataki', 'George','','Republican'),
-	('Rand Paul', ARRAY['Rand Paul'], 'Paul', 'Rand','','Republican'),
-	('Rick Perry', ARRAY['Rick Perry'], 'Perry', 'Rick','','Republican'),
-	('Rick Santorum', ARRAY['Rick Santorum'], 'Santorum', 'Rick','','Republican'),
-	('Joe Biden', ARRAY['Joe Biden'], 'Biden', 'Joe','','Democrat'),
-	('Lincoln Chafee', ARRAY['Lincoln Chafee'], 'Chafee', 'Lincoln','','Democrat'),
-	('Bill de Blasio', ARRAY['Bill de Blasio'], 'de Blasio', 'Bill','','Democrat'),
-	('Jim Webb', ARRAY['Jim Webb'], 'Webb', 'Jim','','Democrat'),
-	('Donald Trump', ARRAY['Donald Trump', 'The Don', 'Sir Trump', 'His Trumpness'], 'Trump', 'Donald','','Weasel');"""
-)
+query = "INSERT INTO Candidates(fullName, validNames, first, last, middle, party) VALUES (%s, %s, %s, %s, %s, %s);"
 
+# data = ('Marco Rubio', ['Marco Rubio', 'Senator Marco Rubio'],'Marco', 'Rubio','','Republican')
+data = (
+	('Marco Rubio', ['Marco Rubio', 'Senator Marco Rubio'],'Marco', 'Rubio','','Republican'),
+	('Carly Fiorina', ['Carly Fiorina', 'Mrs. Carly Fiorina'], 'Fiorina', 'Carly','','Republican'),
+	('Ted Cruz', ['Ted Cruz', 'Senator Ted Cruz'], 'Cruz', 'Ted','','Republican'),
+	('Scott Walker', ['Scott Walker', 'Gov. Scott Walker', 'Governor Scott Walker'], 'Walker', 'Scott','','Republican'),
+	('Bernie Sanders', ['Bernie Sanders', 'Senator Bernie Sanders', 'Bernard Sanders'], 'Sanders', 'Bernie','','Democrat'),
+	('Hillary Rodham Clinton', ['Hillary Rodham Clinton', 'Hillary Clinton'], 'Clinton', 'Hillary', 'Rodham', 'Democrat'),
+	('John R. Kasich', ['John R. Kasich', 'Governor John R. Kasich', 'John Kasich', 'Governor John Kasich'], 'Kasich', 'John', 'R.', 'Republican'),
+	('Chris Christie', ['Chris Christie', 'Governor Chris Christie'], 'Christie', 'Chris','','Republican'),
+	('Ben Carson', ['Ben Carson', 'Benjamin Carson, Sr.', 'Dr. Ben Carson', 'Ben Carson, M.D.'], 'Carson', 'Ben','','Republican'),
+	('Jeb Bush', ['Jeb Bush'], 'Bush', 'Jeb','','Republican'),
+	('Jim Gilmore', ['Jim Gilmore'], 'Gilmore', 'Jim','','Republican'),
+	('Lindsey Graham', ['Lindsey Graham'], 'Graham', 'Lindsey','','Republican'),
+	('Mike Huckabee', ['Mike Huckabee'], 'Huckabee', 'Mike','','Republican'),
+	('Bobby Jindal', ['Bobby Jindal'], 'Jindal', 'Bobby','','Republican'),
+	('George Pataki', ['George Pataki'], 'Pataki', 'George','','Republican'),
+	('Rand Paul', ['Rand Paul'], 'Paul', 'Rand','','Republican'),
+	('Rick Perry', ['Rick Perry'], 'Perry', 'Rick','','Republican'),
+	('Rick Santorum', ['Rick Santorum'], 'Santorum', 'Rick','','Republican'),
+	('Joe Biden', ['Joe Biden'], 'Biden', 'Joe','','Democrat'),
+	('Lincoln Chafee', ['Lincoln Chafee'], 'Chafee', 'Lincoln','','Democrat'),
+	('Donald Trump', ['Donald Trump', 'The Don', 'Sir Trump', 'His Trumpness'], 'Trump', 'Donald','','Weasel'),
+	("Martin J. O'Malley", ["Martin O'Malley"], "O'Malley", 'Martin','J.','Democrat'),
+	('Jim Webb', ['Jim Webb'], 'Webb', 'Jim','','Democrat'),
+	('Bill de Blasio', ['Bill de Blasio'], 'de Blasio', 'Bill','','Democrat')
+	)
+cur.executemany(query, data)
 
 conn.commit()
 
