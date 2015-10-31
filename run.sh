83c07c63b1a760733c6e5801ce965930b1ad4383
Alex Stein <astein.ny@gmail.com>
2015-10-31 18:00:13 -0500

    added job monitor to run.sh

--- run.sh
+++ run.sh
@@ -8,3 +8,13 @@ pushd ./wesaidshesaid/Crawler > /dev/null
 scrapy crawl cspan &
 popd > /dev/null
 
+# when scrapy job is finished, let the user know
+RUNNING=true
+while [ $RUNNING ]; do
+    if [ -z "$(pgrep scrapy)" ]
+    then
+        echo -ne "\n-I- WEB SCRAPING JOB COMPLETE\n"
+        let RUNNING=false
+        break
+    fi 
+done &
